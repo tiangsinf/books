50ab17aa4fc51af8388559ef02389d6707172c55
tiangsinf <tiangsin.foo@gmail.com>
2019-07-28 17:47:22 +0800

    update session

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template, redirect, url_for, request
+from flask import Flask, session, render_template, redirect, url_for, request, abort, Response
 from flask_session import Session
 from dotenv import load_dotenv
 from config import config
@@ -23,46 +23,65 @@ def make_shell_context():
 from forms import LoginForm, RegistrationForm
 @app.route('/')
 def goto_login():
-    return redirect(url_for('index'))
+    if session['SESSIONUSER'] is None:
+        return redirect(url_for('login'))
+    
+    return redirect(url_for('books'))
 
 @app.route('/login', methods=['GET', 'POST'])
-def index():
+def login():
     form = LoginForm()
-    if form.validate_on_submit() is True: 
-        # write to session (key : value pairs) 
-        session['email'] = form.email.data 
-        session['password'] = form.password.data
-        return redirect(url_for('index'))
-    # post/redirect/get
-    return render_template('login.html', form=form, 
-                        email=session.get('email'), 
-                        password=session.get('password'))
 
-@app.route('/register/', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
+    # When user submits form
+    if form.validate_on_submit():
+
+        # Record keyed in email and password to session
+        session['loginer'] = form.email.data # To replace html placeholder when email is submitted
+
+        session.pop('not_registered', None)
+        session.pop('wrong_password', None)
+
+        # Get user by email
+        user = User.query.filter_by(email = form.email.data).scalar()
+        
+        # If user (email) does not exist:
+        if user is None:
+            session.pop('password', None)
+
+            session['not_registered'] = True
+            return redirect('login')
+
+        # If user (email) is found but password not matched:
+        elif user.password != form.password.data:
+            session.pop('password', None)
+
+            session['wrong_password'] = True
+            return redirect('login')
+        
+        # All matched
+        else:
+            # Set user to session user:
+            session['SESSIONUSER'] = user
+            return redirect(url_for('books')) 
     
-    if 'email' in session:
-        email = session.get('email')
-    
-    if form.validate_on_submit() is True:
-        session['name'] = form.name.data
-        session['email'] = form.email.data
-        session['password'] = form.password.data
-        return redirect(url_for('register'))
+    return render_template(
+        'login.html', 
+        form = form, 
+        not_registered = session.get('not_registered'), 
+        wrong_password = session.get('wrong_password')
+    )
 
+@app.route('/books')
+def books():
+    if session['SESSIONUSER'] is None:
+        return redirect(url_for('login'))
 
-    return render_template('register.html', form=form, 
-                        name=session.get('name'),
-                        email=session.get('email'),
-                        password=session.get('password'))
+    return render_template('books.html', user = session.get('SESSIONUSER'))
 
 @app.route('/logout/')
 def logout():
-    session.pop('email')
-    session.pop('name')
-
-    return redirect(url_for('index'))
+    session.pop('SESSIONUSER', None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run()
