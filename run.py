97a67a19c4f45dc3296566d7457f098c3d2ab13b
tiangsinf <tiangsin.foo@gmail.com>
2019-07-31 22:26:39 +0800

    all links setup

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+# 0.0 IMPORT REQUIRED PACKAGES
 import os
 
 from flask import Flask, session, render_template, redirect, url_for, request, abort, Response
@@ -5,26 +6,35 @@ from flask_session import Session
 from dotenv import load_dotenv
 from config import config
 
+# 0.1 LOAD ENVIRONMENTS
 load_dotenv()
+
+# 0.2 INITIATE APP FROM CONFIG
 app = Flask(__name__)
 app.config.from_object(config['development'])
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
 Session(app)
 
+# 0.3 CHECK ENV:
+if not os.getenv('DATABASE_URL'):
+    raise RuntimeError('DATABASE_URL is not set')
+if not os.getenv('FLASK_APP'):
+    raise RuntimeError('FLASK_APP is not set')
+
+# 0.4 MAKE CONTEXT PROCESSOR
 from models import *
 @app.shell_context_processor
 def make_shell_context():
     return dict(app=app, db=db, User=User, Book=Book)
 
+# 1.0 HOME ROUTE
+# user not logged in
 from forms import LoginForm, RegistrationForm, SearchForm
 @app.route('/')
 def goto_login():
     return redirect(url_for('books'))
 
+# 2.0 GET USER LOGIN
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -68,45 +78,19 @@ def login():
         wrong_password = session.get('wrong_password')
     )
 
-@app.route('/books')
-def books():
-    if not session.get('SESSIONUSER'):
-        return redirect(url_for('login'))
-    
-    form = SearchForm()
-    
-    page = request.args.get('page', 1, type=int)
-    books = Book.query.order_by(Book.title).paginate(
-        page,
-        app.config['POST_PER_PAGE'],
-        False
-    )
-
-    return render_template(
-        'books.html', 
-        user=session.get('SESSIONUSER'), 
-        form=form,
-        books=books.items,
-        pages = books.iter_pages(left_edge=1, right_edge=1, left_current=1, right_current=2)
-    )
-
-#! START HERE!
-@app.route('/books/<string:author>')
-def book_author(author):
-    page = request.args.get('page', 1, type=int)
-    author = Book.query.filter_by(author=author).first_or_404()
-    books = Book.query.filter(Book.author==author).all()
-    
-    render_template('author_books.html', author=author)
-
+# 2.1 LOGOUT
 @app.route('/logout/')
 def logout():
+
+    # clear user session
     session.pop('SESSIONUSER', None)
     return redirect(url_for('login'))
 
+# 2.2 REGISTRATION FOR NEW USER
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 
+    # render registration form
     form = RegistrationForm()
     
     if request.method == 'POST':
@@ -117,6 +101,7 @@ def register():
         # Check if email exist:
         user = User.query.filter_by(email=session.get('email')).scalar()
 
+        # Allow user to register
         if user is None:
             new_user = User(session.get('name'), session.get('email'), session.get('password'))
             db.session.add(new_user)
@@ -125,7 +110,8 @@ def register():
                 return redirect('success')
             else:
                 return redirect('fail')
-            
+
+        # Don't allow user to register (email exist)    
         return redirect('used_email')
 
     return render_template(
@@ -133,13 +119,75 @@ def register():
         form=form
     )
 
+# 2.3 USER REGISTRATION UNSUCCESSFUL - NEW USER EMAIL EXIST
 @app.route('/used_email')
 def used_email():
     return render_template('user_exist.html', email=session.get('email'))
 
+# 2.4 USER REGISTRATION SUCCESSFUL
 @app.route('/success')
 def success():
     return render_template('user_registered.html', name=session.get('name'), email=session.get('email'))    
+
+# 3.0 DEFAULT PAGE FOR LOGGED-IN USER
+@app.route('/books')
+def books():
+
+    # if user not logged in, redirect to login page
+    if not session.get('SESSIONUSER'):
+        return redirect(url_for('login'))
+    
+    # user logged in
+    # render search form
+    form = SearchForm()
+    
+    # list all books with pagination
+    page = request.args.get('page', 1, type=int) # grab page from URL
+    books = Book.query.order_by(Book.title).paginate(
+        page = page,
+        per_page = app.config['POST_PER_PAGE']
+    )
+
+    href_url = "\{\{ url_for('author', author=author, page=page) \}\}"
+
+    return render_template('books.html', user=session.get('SESSIONUSER'), form=form, books=books, href_url=href_url)
+
+# 3.1 GET BOOKS FROM AUTHOR
+# if user click on author name from /books -> list all books from author:
+@app.route('/author/<string:author>')
+def author(author):
+
+    # render search form
+    form = SearchForm()
+
+    # list all books published by same author (book_author)
+    page = request.args.get('page', 1, type=int)
+
+    # check if author exist
+    author = Book.query.filter_by(author=author).first_or_404().author
+
+    # fetch books by author
+    books = Book.query.filter_by(author=author).order_by(Book.year.desc()).paginate(
+        page,
+        app.config['POST_PER_PAGE'],
+        False
+    )
+
+    return render_template('author_books.html', 
+                user=session.get('SESSIONUSER'), 
+                form=form, 
+                books=books, 
+                author=author)
+
+# 3.2 GET BOOK BY ISBN
+# if user click on book title or book isbn number from /books -> render book info and user comments
+@app.route('/isbn/<string:book_isbn>')
+def book_isbn(book_isbn):
+    
+    # checks if author exists
+    book = Book.query.filter_by(isbn=book_isbn).first_or_404()
+    
+    return render_template('book.html', book=book)
 
 if __name__ == '__main__':
     app.run()
